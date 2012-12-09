c99dc89bee13b10f948bb3ef865f1d0e9920cbd8
Elmer Bulthuis <elmerbulthuis@gmail.com>
2012-12-09 17:30:17 +0100

    fixing bugs and adding tests

--- test/async.js
+++ test/async.js
@@ -1,19 +1,5 @@
 var la = require('la');
 
-module.exports['sum sync'] = function(beforeExit, assert){
-	var one = la(1);
-	var two = la(2);
-	var sum = la(one, two, function(one, two, cb){
-		cb(null, one + two);
-	});
-
-	sum.get(function(err, value){
-		assert.ifError(err);
-		assert.equal(value, 3);
-	});
-
-}
-
 
 module.exports['sum async'] = function(beforeExit, assert){
 	var one = la(1);
---
+++ test/sync.js
@@ -0,0 +1,42 @@
+var la = require('la');
+
+module.exports['sum sync'] = function(beforeExit, assert){
+	var one = la(1);
+	var two = la(2);
+	var sum = la(one, two, function(one, two, cb){
+		cb(null, one + two);
+	});
+
+	sum.get(function(err, value){
+		assert.ifError(err);
+		assert.equal(value, 3);
+	});
+
+}
+
+
+module.exports['sum sync 2'] = function(beforeExit, assert){
+	var one = la(1);
+	var two = la(2);
+	var sum = la(one, two, function(one, two, cb){
+		cb(null, one + two);
+	});
+
+	sum.get(function(err, value){
+		assert.ifError(err);
+		assert.equal(value, 3);
+	});
+
+	one.set(3)
+
+	sum.get(function(err, value){
+		assert.ifError(err);
+		assert.equal(value, 5);
+	});
+
+}
+
+
+
+
+
