ca551c8391be633347e1c718697e2e4f0c898bb8
Elmer Bulthuis <elmerbulthuis@gmail.com>
2012-12-09 18:19:22 +0100

    adds tests, fixes code

--- test/async.js
+++ test/async.js
@@ -23,18 +23,25 @@ module.exports['sum async'] = function(beforeExit, assert){
 module.exports['sum async 2'] = function(beforeExit, assert){
 	var one = la(1);
 	var two = la(2);
+
+	var countOne = 0;
+	var countTwo = 0;
+	var countSum = 0;
 	
 	one = la(one, function(one, cb){
+		countOne++;
 		setTimeout(function(){
 			cb(null, one);
 		}, 1000);
 	});
 	two = la(two, function(two, cb){
+		countTwo++;
 		setTimeout(function(){
 			cb(null, two);
 		}, 1000);
 	});
 	var sum = la(one, two, function(one, two, cb){
+		countSum++;
 		setTimeout(function(){
 			cb(null, one + two);
 		}, 1000);
@@ -45,8 +52,12 @@ module.exports['sum async 2'] = function(beforeExit, assert){
 		assert.equal(value, 3);
 	});
 
+	beforeExit(function(){
+		assert.equal(countOne, 1);
+		assert.equal(countTwo, 1);
+		assert.equal(countSum, 1);
+	});
 }
-
 
 
 
--- test/sync.js
+++ test/sync.js
@@ -42,3 +42,80 @@ module.exports['sum sync 2'] = function(beforeExit, assert){
 
 
 
+module.exports['sum sync 3'] = function(beforeExit, assert){
+	var firstValue = la(1);
+	var secondValue = la(2);
+
+	var countFirst = 0;
+	var countSecond = 0;
+	var countThird = 0;
+	
+	var firstDependency = la(firstValue, function(first, cb){
+		countFirst++;
+		cb(null, first);
+	});
+	var secondDependency = la(secondValue, function(second, cb){
+		countSecond++;
+		cb(null, second);
+	});
+	var thirdDependency = la(firstDependency, secondDependency, function(first, second, cb){
+		countThird++;
+		cb(null, first + second);
+	});
+
+	thirdDependency.get(function(err, value){
+		assert.ifError(err);
+		assert.equal(value, 3);
+	});
+	assert.equal(countFirst, 1);
+	assert.equal(countSecond, 1);
+	assert.equal(countThird, 1);
+
+
+	firstValue.set(3);
+	thirdDependency.get(function(err, value){
+		assert.ifError(err);
+		assert.equal(value, 5);
+	});
+	assert.equal(countFirst, 2);
+	assert.equal(countSecond, 1);
+	assert.equal(countThird, 2);
+
+
+	secondValue.set(5);
+	thirdDependency.get(function(err, value){
+		assert.ifError(err);
+		assert.equal(value, 8);
+	});
+	assert.equal(countFirst, 2);
+	assert.equal(countSecond, 2);
+	assert.equal(countThird, 3);
+
+
+	secondValue.set(5);
+	thirdDependency.get(function(err, value){
+		assert.ifError(err);
+		assert.equal(value, 8);
+	});
+	assert.equal(countFirst, 2);
+	assert.equal(countSecond, 2);
+	assert.equal(countThird, 3);
+
+	
+	secondValue.set(7);
+	thirdDependency.get(function(err, value){
+		assert.ifError(err);
+		assert.equal(value, 10);
+	});
+	assert.equal(countFirst, 2);
+	assert.equal(countSecond, 3);
+	assert.equal(countThird, 4);
+
+
+}
+
+
+
+
+
+
