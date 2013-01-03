2e99fe60c2c7511b8c8ccaef1f2ce4cc166070d9
Elmer Bulthuis <elmerbulthuis@gmail.com>
2013-01-03 18:03:55 +0100

    adds tests

---
+++ test/error.js
@@ -0,0 +1,100 @@
+var ValueFuture = require('../lib/ValueFuture');
+var LazyFuture = require('../lib/LazyFuture');
+
+
+module.exports['error event'] = function(beforeExit, assert){
+	var counter = 0;
+	var firstLa = new ValueFuture(1);
+	var secondLa = new LazyFuture(firstLa, function(firstValue, cb){
+		setTimeout(function(){
+			cb('error!', firstLa);
+		}, 10);
+	}, true);
+
+
+	beforeExit(function(){
+		assert.equal(counter, 1);
+	});
+
+	secondLa.on('error', function(err){
+		counter++;
+		assert.equal('error!', err)
+	});
+
+	secondLa.get(function(value){
+		counter++;
+	});
+	secondLa.get(function(value){
+		counter++;
+	});
+	secondLa.get(function(value){
+		counter++;
+	});
+
+};
+
+
+module.exports['error cb'] = function(beforeExit, assert){
+	var counter = 0;
+	var firstLa = new ValueFuture(1);
+	var secondLa = new LazyFuture(firstLa, function(firstValue, cb){
+		setTimeout(function(){
+			cb('error!', firstLa);
+		}, 10);
+	}, true);
+
+	beforeExit(function(){
+		assert.equal(counter, 3);
+	});
+
+	secondLa.get(function(err, value){
+		counter++;
+		assert.equal('error!', err)
+	});
+	secondLa.get(function(err, value){
+		counter++;
+		assert.equal('error!', err)
+	});
+	secondLa.get(function(err, value){
+		counter++;
+		assert.equal('error!', err)
+	});
+
+};
+
+
+
+module.exports['error both'] = function(beforeExit, assert){
+	var counter = 0;
+	var firstLa = new ValueFuture(1);
+	var secondLa = new LazyFuture(firstLa, function(firstValue, cb){
+		setTimeout(function(){
+			cb('error!', firstLa);
+		}, 10);
+	}, true);
+
+
+	beforeExit(function(){
+		assert.equal(counter, 4);
+	});
+
+	secondLa.on('error', function(err){
+		counter++;
+		assert.equal('error!', err)
+	});
+
+	secondLa.get(function(err, value){
+		counter++;
+		assert.equal('error!', err)
+	});
+	secondLa.get(function(err, value){
+		counter++;
+		assert.equal('error!', err)
+	});
+	secondLa.get(function(err, value){
+		counter++;
+		assert.equal('error!', err)
+	});
+
+};
+
